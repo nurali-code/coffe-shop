2dbb3bd999ac5e3df800cbffe695db1f8641b7fd
nurali-code <nur3.dav.97@gmail.com>
2024-03-06 17:38:13 +0500

    menu

--- js/script.js
+++ js/script.js
@@ -3,54 +3,21 @@ $('.btn_menu').on('click', function () {
     $('#nav, .btn_menu, body').toggleClass('is_active');
 })
 
-// document.addEventListener('click', function (e) {
-//     if (!e.target.closest('#nav') && e.target !== btn_menu) {
-//         navbar.classList.remove('is_active');
-//         btn_menu.classList.remove('is_active');
-//     }
-// });
-
-function hideModals() {
-    $('.modal').removeClass('is_active').fadeOut();
-    $('body, .header .nav, .btn_menu').removeClass('is_active');
-};
-
-$(function () {
-    function showModal(id) {
-        $(id).addClass('is_active').fadeIn(300);
-        $(id).addClass('is_active');
-    }
-
-    $('[data-modal]').on('click', function (e) {
-        e.preventDefault();
-        showModal('#' + $(this).attr("data-modal"));
-    });
-
-    $('.modal-close').on('click', () => {
-        hideModals();
-    });
-
-    $(document).on('click', function (e) {
-        if (!(
-            ($(e.target).parents('.modal-content').length) ||
-            ($(e.target).hasClass('btnModal')) ||
-            ($(e.target).hasClass('btn')) ||
-            ($(e.target).hasClass('nav')) ||
-            ($(e.target).hasClass('nav__link')) ||
-            ($(e.target).hasClass('btn_menu')) ||
-            ($(e.target).hasClass('modal-content'))
-        )) {
-            hideModals();
-        }
-    });
-});
-
-$('.products-slider').slick({
+$('.menu-slider').slick({
     arrows: true,
-    infinite: false,
-    dots: true,
+    infinite: true,
+    dots: false,
+    speed: 500,
+    slidesToShow: 1,
+    centerPadding: '0px',
+    centerMode: true,
+});
+$('.interior-slider').slick({
+    arrows: true,
+    infinite: true,
+    dots: false,
     speed: 300,
-    slidesToShow: 3,
+    slidesToShow: 2,
     centerPadding: '0px',
     // centerMode: true,
     variableWidth: true,
@@ -58,27 +25,11 @@ $('.products-slider').slick({
         {
             breakpoint: 1100,
             settings: {
-                centerPadding: '20px',
                 slidesToShow: 2,
-                variableWidth: false,
-                slidesToScroll: 2,
+                slidesToScroll: 1,
+                centerPadding: '15px',
                 infinite: true,
-                dots: true
             }
-        },
-        {
-            breakpoint: 800,
-            settings: {
-                centerPadding: '20px',
-                slidesToShow: 1,
-                variableWidth: false,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true
-            }
-        },
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
+        }
     ]
 });
